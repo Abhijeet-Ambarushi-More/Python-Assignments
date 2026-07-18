d831685383eb4118564f384bafc58adbb2a996f8
Abhijeet More <112226209+Abhijeet-Ambarushi-More@users.noreply.github.com>
2026-07-19 03:59:02 +0530

    Add files via upload

---
+++ Assignment 28/A28_Q01.py
@@ -0,0 +1,38 @@
+######################################################################################################################
+#
+# Count lines in a file
+# 
+# problem statement : 
+# wap which accepts a file name from the user and counts how many lines are present in the file.
+#
+# Input :
+# Marvellous.txt
+#
+# Expected output : 
+# Total number of lines in Demo.txt
+#
+######################################################################################################################
+def main():
+    try:
+        Count = 0
+
+        fd = open("Marvellous.txt","r",encoding="cp1252")
+
+        while True:
+            line = fd.readline()
+
+            if line == "":
+                break
+
+            Count += 1
+
+        fd.close()
+
+        print("Total number of lines in Marvellous.txt : ",Count)
+
+
+    except FileNotFoundError as fobj:
+        print("File is not present in current directory")
+
+if __name__ == "__main__":
+    main()
